f123056033d8a181316e7796087802aec07ec28f
nguyenlamphuc <126443277+BobbyBin14@users.noreply.github.com>
2023-06-02 13:34:58 +1000

    Add files via upload

---
+++ a3/textcrawl.js
@@ -0,0 +1,7 @@
+function loadPageAfterDelay(loadTime, url) {
+  document.getElementById("load-link").style.display = "none";
+  document.getElementById("load-throbber").style.display = "block";
+  setTimeout(() => {
+    window.location.href = url;
+  }, loadTime);
+}
